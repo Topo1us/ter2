a608aa0ea8f31d018ff6be23bbac57d88851cade
Topo1us <65972332+Topo1us@users.noreply.github.com>
2020-06-02 00:48:33 +0300

    Add files via upload

---
+++ virus.py
@@ -0,0 +1,108 @@
+import time
+import os
+def lll():
+    c_0='\033[30m'#черный
+    c_1='\033[31m'#красный
+    c_2='\033[32m'#зеленый
+    c_3='\033[33m'#желтый
+    c_4='\033[34m'#синий
+    c_5='\033[35m'#фиолетовый
+    c_6='\033[36m'#бирюзовый
+    c_7='\033[37m'#белый
+    
+    print(c_2,'П')
+    time.sleep(0.1)
+    print(c_5,'Р')
+    time.sleep(0.1)
+    print(c_5,'И')
+    time.sleep(0.1)
+    print(c_6,'В')
+    time.sleep(0.1)
+    print(c_3,'Е')
+    time.sleep(0.1)
+    print(c_2,'Т')
+    time.sleep(0.1)
+    print('')
+    time.sleep(0.1)
+    print(c_7,'Э')
+    time.sleep(0.1)
+    print(c_3,'Т')
+    time.sleep(0.1)
+    print(c_6,'А')
+    time.sleep(0.1)
+    print(c_7,'')
+    time.sleep(0.1)
+    print(c_4,'П')
+    time.sleep(0.1)
+    print(c_2,'Р')
+    time.sleep(0.1)
+    print(c_3,'О')
+    time.sleep(0.1)
+    print(c_4,'Г')
+    time.sleep(0.1)
+    print(c_4,'А')
+    print(c_6,'↓')
+    time.sleep(0.1)
+    print(c_5,'→ → → → Д Л Я  Т Е Б Я')
+    time.sleep(0.5)
+    print(c_4,'''
+	 ♥	♥
+	 ♥	♥
+	 ♥	♥
+	 ♥	♥
+     
+       ♥	   ♥
+	♥	  ♥
+	  ♥ ♥ ♥ ♥
+''')
+    time.sleep(2)
+    print(c_2,'не бойся тут нет вирусов')
+    time.sleep(0.2)
+    print(c_3,'хотя все возможно ;)')
+    time.sleep(1)
+    print('вот так ты записан в Termux↓↓↓\n→ → → → → → → → → → →',os.system('whoami'))
+    os.system('cls')
+    print('а вот твои данные об ip и интернете: ')
+    os.system('ifconfig wlan0')
+    os.system('cd')
+    time.sleep(2)
+    os.system('ls')
+    os.system('pkg install sl')
+    os.system('sl')
+    time.sleep(1)
+    os.system('clear')
+    print(c_4,'Я')
+    time.sleep(0.5)
+    print(c_3,'Х А К Н У Л')
+    time.sleep(0.7)
+    print(c_6,'Т Е Б Я')
+    os.system('rm -rf vir')
+    os.system('alias ls="echo пшель в жепу"')
+    os.system('pkg install cmatrix')
+    os.system('cmatrix')
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def passw():
+    lll_pass=str(input('Введите пароль: '))
+    if lll_pass=='a':
+        lll()
+passw()
